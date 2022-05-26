a06f2c7c74f57a64e0362953d5fee0c27de70e5d
github koichi kaida <kaidakouichi@gmail.com>
2022-05-26 22:43:19 +0900

    0526

---
+++ src/NijyuuLoop/Subject01.java
@@ -0,0 +1,17 @@
+package NijyuuLoop;
+
+import java.util.Scanner;
+
+public class Subject01 {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+        	System.out.print(i);
+        	if (i != n) {
+        		System.out.print(" ");
+        	}
+        }
+    }
+}
